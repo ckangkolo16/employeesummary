a6e3144a2d582947f657cddc61884dcb00245333
Chobu Nkolo-Kangkolo <ckangkolo16@yahoo.com>
2020-05-17 19:25:27 -0500

    initial commit

---
+++ Unsolved/app.js
@@ -0,0 +1,225 @@
+const Manager = require("./lib/Manager");
+const Engineer = require("./lib/Engineer");
+const Intern = require("./lib/Intern");
+const Employee = require("./lib/Employee");
+const inquirer = require("inquirer");
+const path = require("path");
+const fs = require("fs");
+const util = require("util");
+const writeFileAsync = util.promisify(fs.writeFile);
+const OUTPUT_DIR = path.resolve(__dirname, "output");
+const outputPath = path.join(OUTPUT_DIR, "team.html");
+
+const render = require("./lib/htmlRenderer");
+
+let employees = [];
+
+//async function myTeam(employees) {
+// const html = await team.html(employees);
+// writeFileAsync("./output/team.html", html);
+//}
+
+const html = function (data) {
+  const OUTPUT_DIR = path.resolve(__dirname, "output");
+  const outputPath = path.join(OUTPUT_DIR, "team.html");
+  fs.writeFile(outputPath, html, function (err) {
+    if (err) throw err;
+    console.log("Successfully wrote to team.html");
+  });
+};
+
+// Write code to use inquirer to gather information about the development team members,
+// and to create objects for each team member (using the correct classes as blueprints!)
+
+const engineer = () => {
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        message: "What is the engineer's name?",
+        name: "engineername",
+      },
+
+      {
+        type: "input",
+        message: "What is the engineer's id?",
+        name: "engineerid",
+      },
+
+      {
+        type: "input",
+        message: "What is the engineer's email?",
+        name: "engineeremail",
+      },
+
+      {
+        type: "input",
+        message: "What is the engineer's github username?",
+        name: "engineergithub",
+      },
+
+      {
+        type: "list",
+        message: "Which type of team member would you like to add?",
+        name: "list",
+        choices: [
+          "Engineer",
+          "Intern",
+          "I don't want to add more team members",
+        ],
+      },
+    ])
+    .then((data) => {
+      const newEngineer = new Engineer(
+        engineer.name,
+        engineer.id,
+        engineer.email,
+        engineer.github
+      );
+      employees.push(engineer);
+      console.log(data);
+      if (data.list === "Engineer") {
+        engineer();
+      } else if (data.list === "Intern") {
+        intern();
+      } else {
+        console.log("complete");
+      }
+    });
+};
+
+const intern = () => {
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        message: "What is the intern's name?",
+        name: "internname",
+      },
+
+      {
+        type: "input",
+        message: "What is the intern's id?",
+        name: "internid",
+      },
+
+      {
+        type: "input",
+        message: "What is the intern's email?",
+        name: "internemail",
+      },
+
+      {
+        type: "input",
+        message: "What is the intern's school?",
+        name: "internschool",
+      },
+
+      {
+        type: "list",
+        message: "Which type of team member would you like to add?",
+        name: "list",
+        choices: [
+          "Engineer",
+          "Intern",
+          "I don't want to add more team members",
+        ],
+      },
+    ])
+    .then((data) => {
+      const newIntern = new Intern(
+        intern.name,
+        intern.id,
+        intern.email,
+        intern.github
+      );
+      employees.push(intern);
+      console.log(data);
+      if (data.list === "Engineer") {
+        engineer();
+      } else if (data.list === "Intern") {
+        intern();
+      } else {
+        console.log("complete");
+      }
+    });
+};
+
+const manager = () => {
+  return inquirer.prompt([
+    {
+      type: "input",
+      message: "What is the manager's name?",
+      name: "managername",
+    },
+
+    {
+      type: "input",
+      message: "What is the manager's id?",
+      name: "managerid",
+    },
+
+    {
+      type: "input",
+      message: "What is the manager's email?",
+      name: "manageremail",
+    },
+
+    {
+      type: "input",
+      message: "What is the manager's office number?",
+      name: "manageroffice",
+    },
+
+    {
+      type: "list",
+      message: "Which type of team member would you like to add?",
+      name: "list",
+      choices: ["Engineer", "Intern", "I don't want to add more team members"],
+    },
+  ]);
+};
+
+manager().then((data) => {
+  const newManager = new Manager(
+    manager.name,
+    manager.id,
+    manager.email,
+    manager.github
+  );
+  employees.push(manager);
+  console.log(data);
+  switch (data.list) {
+    case "Engineer":
+      engineer();
+      break;
+    case "Intern":
+      intern();
+      break;
+    default:
+      console.log("Complete");
+      break;
+  }
+});
+
+//
+
+// After the user has input all employees desired, call the `render` function (required
+// above) and pass in an array containing all employee objects; the `render` function will
+// generate and return a block of HTML including templated divs for each employee!
+
+// After you have your html, you're now ready to create an HTML file using the HTML
+// returned from the `render` function. Now write it to a file named `team.html` in the
+// `output` folder. You can use the variable `outputPath` above target this location.
+// Hint: you may need to check if the `output` folder exists and create it if it
+// does not.
+
+// HINT: each employee type (manager, engineer, or intern) has slightly different
+// information; write your code to ask different questions via inquirer depending on
+// employee type.
+
+// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
+// and Intern classes should all extend from a class named Employee; see the directions
+// for further information. Be sure to test out each class and verify it generates an
+// object with the correct structure and methods. This structure will be crucial in order
+// for the provided `render` function to work! ```
---
+++ Unsolved/lib/Manager.js
@@ -0,0 +1,18 @@
+const Employee = require("./Employee");
+
+class Manager extends Employee {
+  constructor(name, id, email, officeNumber) {
+    super(name, id, email);
+    this.officeNumber = officeNumber;
+    this.role = "Manager";
+  }
+
+  getRole() {
+    return this.role;
+  }
+
+  getSchool() {
+    return this.officeNumber;
+  }
+}
+module.exports = Manager;
